d554905b51ed9ece412e45ba5d7b078c28c4b67a
Stephan Scholz <stephan_ka@gmx.net>
2017-06-22 14:37:05 +0200

    Started working on implementation branch. Added Template structure for the Transport module (not filled yet) and for Transport.h. In Actuators.h: Added Ice_t and Transport_t with placeholders which need to be linked to the pins later to control the motors of these moduls.

--- Inc/Actuators.h
+++ Inc/Actuators.h
@@ -25,7 +25,19 @@ typedef struct {
 }Pouring_t;
 
 
+//Muss noch Hardwaremaessig umgesetzt werden, hier als Platzhalter fuer was wir brauchen
+typedef struct {
+	uint_8 motor;
+	uint_8 startUp []; //Array fuer Werte um langsames Anfahren des Transportbandes zu ermoeglichen
+	uint_8 windDown []; //Array fuer Werte um langsames stoppen des Transportbandes zu ermoeglichen
+	bool LED_Status //LED die leuchten soll wenn das Transportband steht, damit man ein neues Glas reinstellen kann
+}Transport_t;
 
+//Muss noch Hardwaremaessig umgesetzt werden, hier als Platzhalter fuer was wir brauchen
+typedef struct {
+	uint_8 motor;
+	float motorAngle; //Winkelmesser am Motor zum feststellen ob er sich dreht oder feststeckt
+}Ice_t;
 
 //Define the Type where all output values will be stored later
 typedef struct {
@@ -34,6 +46,8 @@ typedef struct {
 	Gravity_t Gravity;
 	Pumping_t Pumping;
 	Pouring_t Pouring;
+	Transport_t Transport;
+	Ice_t Ice;
 	int allow_modules[7]; // Anweisung an die Module, ob erlaubt mit Arbeit zu beginnen.
 	int placeholder_handling;
 	int placeholder_ice;
