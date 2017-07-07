8086220e4af73956ef54e09064d6c304e5c16b22
Olaf Dünkel <urfbk@student.kit.edu>
2017-07-07 21:22:16 +0200

    Sensoren eingetragen

--- Src/Sensors.c
+++ Src/Sensors.c
@@ -87,30 +87,62 @@ void vReadSensorValues (InputValues_t* input)
 	//Read the Button State (Pin 13 at GPIO C)
     ptrInput->Button = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13);
 	//read, and save the inputs here
+    ptrInput->Transportation.start = 
+    ptrInput->Transportation.newGlas = 
+    ptrInput->Transportation.inModule2
+	ptrInput->Transportation.inModule3
+	ptrInput->Transportation.inModule4
+	ptrInput->Transportation.inModule7
+	
+	return;
 }
 
  void vReadSensorValues_Module_2()
 {
+	ptrInput->Gravity.weight_sensor
+	ptrInput->Gravity.doors_open
+	ptrInput->Gravity.sensor_up
+	ptrInput->Gravity.sensor_down
+	ptrInput->Gravity.button_fill_in
+	ptrInput->Gravity.position_tree
+	
 	return;
 }
 
  void vReadSensorValues_Module_3()
 {
+	 ptrInput->Pumping.doors_open
+	 ptrInput->Pumping.valve_position
+	 ptrInput->Pumping.weight_glass
 	return;
 }
 
  void vReadSensorValues_Module_4()
 {
+	 ptrInput->Pouring.doors_open
+	 ptrInput->Pouring.position_up
+	 ptrInput->Pouring.position_down
+	 ptrInput->Pouring.weight
 	return;
 }
 
  void vReadSensorValues_Module_5()
 {
+	 ptrInput->Sensors.start_doors_open
+	 ptrInput->Sensors.start_light_barrier
+	 ptrInput->Sensors.end_doors_open
+	 ptrInput->Sensors.end_light_barrier
+	 ptrInput->Sensors.end_button_glass_present
 	return;
 }
 
  void vReadSensorValues_Module_6()
 {
+	 ptrInput->Ice.weight
+	 ptrInput->Ice.doors_open
+	 ptrInput->Ice.light_barrier_enough_cube_ice
+	 ptrInput->Ice.light_barrier_enough_crushed_ice
+	 
 	return;
 }
 
